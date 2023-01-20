998983b6b33e981b2a7ebb210c37c1bfa4ae3aeb
Tom Shephard <tomy.shephard@gmail.com>
2023-01-20 15:10:52 +0000

    Added function declaration for createWindow

--- chess.cpp
+++ chess.cpp
@@ -1,31 +1,31 @@
 #include <GL/glut.h>
 
 
-void createWindow(int argc, char **argv, char* windowName){
-    
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-    glutInitWindowPosition(100,100);
-    glutInitWindowSize(800, 800);
-    glutCreateWindow(windowName);
-
-}
-
+void createWindow(int argc, char **argv);
 
 int main (int argc, char **argv){
 
     int loop = 1;
-    int a = argc;
     char ** b = argv;
     
-    createWindow(a, b, "I love Cum");
+    createWindow(argc, b);
     
     
     while(loop){
 
-    
+        /*Does nish just keeps the window open*/
+
     }
 
     return 1;
 }
 
+void createWindow(int argc, char **argv){
+    
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowPosition(100,100);
+    glutInitWindowSize(800, 800);
+    glutCreateWindow("Chess");
+
+}
